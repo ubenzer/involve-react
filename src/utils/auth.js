c86466c3dfb7d89b221ffa2f466e8bcf66caaeab
Umut Benzer <umutbenzer@gmail.com>
2016-11-10 23:55:50 +0100

    fix(auth): Anonymous user id is lost on refresh

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,10 +1,27 @@
+import firebase from "firebase";
+
 export function ensureAnonymousLogin(store, onSuccess, onFail) {
   const state = store.getState();
   if (state.firebase.auth) {
+    // dude is already logged
     onSuccess();
-  } else {
-    store.firebase.auth().signInAnonymously()
+  } else if (state.firebase.auth === null) {
+    // dude is not logged in yet, lets log him in
+    firebase.auth().signInAnonymously()
       .then(onSuccess)
       .catch(onFail);
+  } else {
+    // we don't know the login status yet, let's wait for it
+    let cancelFn = firebase.auth()
+      .onAuthStateChanged((auth) => {
+        cancelFn();
+        if (auth) {
+          onSuccess();
+        } else {
+          firebase.auth().signInAnonymously()
+            .then(onSuccess)
+            .catch(onFail);
+        }
+      }, onFail);
   }
 }
